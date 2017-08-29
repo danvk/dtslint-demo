c40a7e4b54a1ce4f10b17b3d8319e01b9411a8dc
Dan Vanderkam <danvdk@gmail.com>
2017-08-29 19:03:16 -0400

    Cleaner module structure

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,8 +1,3 @@
 // TypeScript Version: 2.1
 
-interface Utils {
-  map<T, U>(xs: T[], fn: (x: T) => U): U[];
-}
-
-declare const utils: Utils;
-export = utils;
+export function map<T, U>(xs: T[], fn: (x: T) => U): U[];
